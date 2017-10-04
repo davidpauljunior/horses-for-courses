d59940c7fba07260614060943600b7ae10929c85
David Paul <davidpauljunior@gmail.com>
2017-10-04 22:30:52 +0100

    :construction: Bgins trying to query races to display on home page - currently broken check logs

--- models/Race.js
+++ models/Race.js
@@ -1,14 +1,14 @@
-
 var keystone = require('keystone');
 var Types = keystone.Field.Types;
 
 var Race = new keystone.List('Race');
 
+// format: 'D MMM YYYY hh:mm' this isn't working in datetime
+
 Race.add({
-    date: { type: Types.Date, required: true, initial: true },
-    course: {type: String, required: true, initial: true },
-    race: { type: Types.Date, format: 'hh:mm', required: true, initial: true },
-    odds: { type: String, required: true, initial: true},
+    date: { type: Types.Datetime, required: true, initial: true },
+    course: {type: Types.Text, required: true, initial: true },
+    odds: { type: Types.Text, required: true, initial: true},
 	position: { type: Types.Select, options: [
         { value: 'won', label: 'Won' },
         { value: 'placed', label: 'Placed' },
--- routes/views/index.js
+++ routes/views/index.js
@@ -1,4 +1,5 @@
 var keystone = require('keystone');
+var race = keystone.list('Race');
 
 exports = module.exports = function (req, res) {
 
@@ -8,6 +9,40 @@ exports = module.exports = function (req, res) {
 	// locals.section is used to set the currently selected
 	// item in the header navigation.
 	locals.section = 'home';
+	locals.races = [];
+
+	// Load the races
+	view.on('init', function (next) {
+		var q = Race.paginate({
+			page: req.query.page || 1,
+			perPage: 10,
+			maxPages: 10,
+		})
+		.where('state', 'published')
+		.sort('-publishedDate');
+
+		q.exec(function (err, results) {
+			locals.races = results;
+			next(err);
+		});
+		// var q = Post.paginate({
+		// 		page: req.query.page || 1,
+		// 			perPage: 10,
+		// 			maxPages: 10,
+		// 	})
+		// 	.where('state', 'published')
+		// 	.sort('-publishedDate')
+		// 	.populate('author categories');
+
+		// if (locals.category) {
+		// 	q.where('categories').in([locals.category]);
+		// }
+
+		// q.exec(function (err, results) {
+		// 	locals.posts = results;
+		// 	next(err);
+		// });
+	});
 
 	// Render the view
 	view.render('index');
