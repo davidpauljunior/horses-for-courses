141a9dcf66e1ba714ea8e42f86cd0b04e5228fda
David Paul <davidpauljunior@gmail.com>
2017-10-09 22:58:42 +0100

    Begins adding decimal odds

--- routes/views/index.js
+++ routes/views/index.js
@@ -10,15 +10,35 @@ exports = module.exports = function (req, res) {
 	// item in the header navigation.
 	locals.section = 'home';
 	locals.races = [];
+	
+	// TODO: create an updated results and return that as the local.races array.
+
+	function decimalOdds(fraction) {
+		const splitFields = fraction.split('/');
+		return ((splitFields[0] / splitFields[1]) + 1).toFixed(2);
+	}
 
 	view.on('init', function(next) {
-		// TODO: Pagination
+		// TODO later: Pagination
 		var q = Race.model.find();
 
-		q.exec(function(err, results) {
-			console.log(results); // empty array
+		q.exec(function(err, results) { 
+			var updatedResults = [];
+
+			for (var i = 0; i < results.length; i++) {
+				var result = results[i];
+				var odds = decimalOdds(result.odds);
+				result.decimalOdds = odds;
+
+				updatedResults.push(result);
+				console.log(result.decimalOdds);
+			}
+
+			// THIS DOES NOT CONTAIN THE DECIMAL ODDS
+			console.log(updatedResults);
+
 			// do something with posts
-			locals.races = results;
+			locals.races = results; // TODO: Will change to updatedResults
 			next(err);
 		});
 	});
